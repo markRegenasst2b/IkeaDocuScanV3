7099f40967a70c2333e80353d8551277e10b85a7
Marki <markregenass@yahoo.de>
2025-11-25 15:20:16 +0100

    failed publish

--- IkeaDocuScanV3/IkeaDocuScan-Web/IkeaDocuScan-Web.Client/Constants.cs
+++ IkeaDocuScanV3/IkeaDocuScan-Web/IkeaDocuScan-Web.Client/Constants.cs
@@ -1,5 +1,5 @@
 ﻿namespace IkeaDocuScan_Web.Client;
 public static class Constants 
 { 
-    public const string DisplayVersion = "3.1.19-testdeploy1125";
+    public const string DisplayVersion = "3.1.20-testdeploy1125";
 }
