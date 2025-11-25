2e436ecd51a01a4d86f991e0b7ffe6371feb50e7
Marki <markregenass@yahoo.de>
2025-11-25 15:21:15 +0100

    Publish Debug 3.1.21-testdeploy1125

--- IkeaDocuScanV3/IkeaDocuScan-Web/IkeaDocuScan-Web.Client/Constants.cs
+++ IkeaDocuScanV3/IkeaDocuScan-Web/IkeaDocuScan-Web.Client/Constants.cs
@@ -1,5 +1,5 @@
 ﻿namespace IkeaDocuScan_Web.Client;
 public static class Constants 
 { 
-    public const string DisplayVersion = "3.1.20-testdeploy1125";
+    public const string DisplayVersion = "3.1.21-testdeploy1125";
 }
